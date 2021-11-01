5ea7ecaad95d9024289c0ba92b36692f70357d7d
William Galas <william.galas@coreproc.ph>
2021-11-01 13:00:28 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,26 @@
-<h1>
-    Welcome to Nuwang.tech
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print Interface</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <style>
+        @page {
+            size:A4;
+        }
+        @media print {
+            body, html{
+                padding: 0px;
+                margin: 0px;
+            }
+        }
+    </style>
+</head>
+<body class="flex justify-center items-center">
+    <h1 class="font-bold uppercase tracker-2 text-2xl">
+        UNDER DEVELOPMENT
+    </h1>
+</body>
+</html>
