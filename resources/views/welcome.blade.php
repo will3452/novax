ab15223ffbb75188e64850e278fbe1979a1d70b7
William Galas <william.galas@coreproc.ph>
2021-11-04 00:49:24 +0800

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,7 @@
-<h1>
-    Welcome to Nuwang.tech
-</h1>
+<x-layout>
+    <div class="w-screen h-screen flex justify-center items-center bg-gray-100">
+        <h1 class="text-3xl uppercase font-bold text-gray-700">
+            &lt;Under Development /&gt;
+        </h1>
+    </div>
+</x-layout>
