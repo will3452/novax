9f1b34f2fefa5521a48a5e0d9ee8814082904656
William Galas <william.galas@coreproc.ph>
2021-11-08 22:06:42 +0800

    Update 2021_11_05_184106_create_locations_table.php

--- database/migrations/2021_11_05_184106_create_locations_table.php
+++ database/migrations/2021_11_05_184106_create_locations_table.php
@@ -16,7 +16,6 @@ class CreateLocationsTable extends Migration
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->foreignId('user_id')->onDelete('cascade');
             $table->timestamps();
         });
     }
