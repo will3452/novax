45f9375a60a3012c532d1d8a9bb3949011a3026e
William Galas <william.galas@coreproc.ph>
2021-11-09 23:11:00 +0800

    Update logo.blade.php

--- resources/views/vendor/nova/partials/logo.blade.php
+++ resources/views/vendor/nova/partials/logo.blade.php
@@ -1,3 +1,3 @@
 <div style="width:100%; display:flex; justify-content:center;">
-    <img src="/storage/{{nova_get_setting('logo')}}" alt="" style="width:75px;"/>
+    <img src="/storage/{{nova_get_setting('logo')}}" alt="" style="width:75px;border-radius:50%;"/>
 </div>
