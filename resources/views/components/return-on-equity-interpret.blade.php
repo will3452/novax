9c6f189431b0651492d7d74ce28d04897d4500a6
William Galas <william.galas@coreproc.ph>
2021-11-10 03:20:38 +0800

    Update return-on-equity-interpret.blade.php

--- resources/views/components/return-on-equity-interpret.blade.php
+++ resources/views/components/return-on-equity-interpret.blade.php
@@ -1,4 +1,4 @@
-@if (request()->prof_ratio == 'return_on_assets')
+@if (request()->prof_ratio == 'return_on_equity')
 <div class="border-2  p-2 rounded text-xl font-bold">
     The Entity enjoyed {{\App\Accounting::getReturnOnEquity()}} % returns to th shareholdr investments.
 @endif
