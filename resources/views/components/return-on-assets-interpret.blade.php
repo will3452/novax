1e9daf236ca09360cec020ec296cc499e91b2389
William Galas <william.galas@coreproc.ph>
2021-11-10 03:22:46 +0800

    wip

--- resources/views/components/return-on-assets-interpret.blade.php
+++ resources/views/components/return-on-assets-interpret.blade.php
@@ -1,5 +1,5 @@
 @if (request()->prof_ratio == 'return_on_assets')
 <div class="border-2  p-2 rounded text-xl font-bold">
-    The Entity enjoyed {{number_format(\App\Accounting::getReturnOnAssetsRatio())}} % returns in the usage of its assets to generate profit.
+    The Entity enjoyed {{number_format(\App\Accounting::getReturnOnAssetsRatio(),2)}} % returns in the usage of its assets to generate profit.
 </div>
 @endif
