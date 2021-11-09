d0e58a290776c4d162924005c05788c2330dce8a
William Galas <william.galas@coreproc.ph>
2021-11-10 01:21:54 +0800

    wip

--- resources/views/components/acid-ratio-interpret.blade.php
+++ resources/views/components/acid-ratio-interpret.blade.php
@@ -1,6 +1,6 @@
 @if (request()->liquidity_ratio == 'acid_ratio')
 <div class="border-2  p-2 rounded text-xl font-bold">
-    @if (\App\Accounting::getAcidRatio() > 1.0)
+    @if (abs(\App\Accounting::getAcidRatio()) > 1)
         The Entity can pay for their current liabilities using their quick assets.
     @else
     The Entity cannot pay for their current liabilities using their quick assets.
--- resources/views/components/cash-ratio-interpret.blade.php
+++ resources/views/components/cash-ratio-interpret.blade.php
@@ -1,9 +1,9 @@
 @if (request()->liquidity_ratio == 'cash_ratio')
 <div class="border-2  p-2 rounded text-xl font-bold">
-    @if (\App\Accounting::getCashRatio() > 1.0)
+    @if (abs(\App\Accounting::getCashRatio()) > 1)
         The Entity can pay for their current liabilities using their total cash.
     @else
-    The Entity cannot pay for their current liabilities using their total cash.
+        The Entity cannot pay for their current liabilities using their total cash.
     @endif
 </div>
 @endif
