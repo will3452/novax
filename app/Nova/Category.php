8f1436f01b47cd59d4952c748e84a363b8698f06
William Galas <william.galas@coreproc.ph>
2021-11-11 09:51:30 +0800

    wip

--- app/Nova/Category.php
+++ app/Nova/Category.php
@@ -4,6 +4,7 @@ namespace App\Nova;
 
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Image;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
@@ -45,6 +46,9 @@ class Category extends Resource
         return [
             Text::make('Category Name', 'name')
                 ->rules(['required', 'unique:categories,name']),
+
+            Image::make('Image')
+                ->rules(['required']),
         ];
     }
 
