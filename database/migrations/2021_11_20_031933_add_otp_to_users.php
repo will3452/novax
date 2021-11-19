3a14a25a7899491bb36aec5a91d974d989a2a333
William Galas <36730333+will3452@users.noreply.github.com>
2021-11-20 03:23:05 +0800

    Create 2021_11_20_031933_add_otp_to_users.php

---
+++ database/migrations/2021_11_20_031933_add_otp_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOtpToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('otp')->default(rand(1000, 9999));
+            $table->string('otp_checked')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('otp', 'otp_checked');
+        });
+    }
+}
