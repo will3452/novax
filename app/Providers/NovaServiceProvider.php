1328e4bcb8b78a2dcac789cd6274533dcbb40f09
William Galas <36730333+will3452@users.noreply.github.com>
2021-11-20 04:28:37 +0800

    Update NovaServiceProvider.php

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -117,7 +117,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
                     'categories' => [Accounting::getAccountingPeriodString()]
                 ],
             ])
-            ->width('1/2'),
+            ->width('full'),
             (new BarChart())
             ->title('Profitability Ratio')
             ->animations([
@@ -140,7 +140,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
                     'categories' => [Accounting::getAccountingPeriodString()]
                 ],
             ])
-            ->width('1/2'),
+            ->width('full'),
 
         ];
     }
