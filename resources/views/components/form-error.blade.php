bfdc515ab3c845eaab87bd52d1a2cb514a905659
William Galas <36730333+will3452@users.noreply.github.com>
2021-11-21 22:52:11 +0800

    wip

---
+++ resources/views/components/form-error.blade.php
@@ -0,0 +1,3 @@
+<div class="text-xs text-red-500 text-center">
+    {{$slot}}
+</div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-theme="cupcake">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{config('app.name', 'Nuwang')}}</title>
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@1.16.2/dist/full.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
     {{$slot}}
