37c33f7fea60b69f8f7360d868c15b64843300b3
William Galas <williamgalas2@gmail.com>
2022-02-01 13:04:37 +0800

    wip

--- app/Http/Controllers/LeadboardController.php
+++ app/Http/Controllers/LeadboardController.php
@@ -11,21 +11,22 @@ class LeadboardController extends Controller
 {
     public function getLeaderboard()
     {
-        $type = request()->get('type') ?? 'topic';
-        $topicId = request()->get('topic_id');
-        $limit= request()->get('limit') ?? 10;
-        $users = null;
-        if ($type != 'topic') {
-            $users = User::withSum('scores', 'score')->orderBy('scores_sum_score', 'DESC')->take($limit)->get();
-        } else {
-            if ($topicId == null) {
-                return ErrorHelper::sendError(404);
-            }
-            $users = Topic::find($topicId)->scores()->orderBy('score', 'DESC')->take($limit)->with('user')->get();
-        }
+        return 1;
+        // $type = request()->get('type') ?? 'topic';
+        // $topicId = request()->get('topic_id');
+        // $limit= request()->get('limit') ?? 10;
+        // $users = null;
+        // if ($type != 'topic') {
+        //     $users = User::withSum('scores', 'score')->orderBy('scores_sum_score', 'DESC')->take($limit)->get();
+        // } else {
+        //     if ($topicId == null) {
+        //         return ErrorHelper::sendError(404);
+        //     }
+        //     $users = Topic::find($topicId)->scores()->orderBy('score', 'DESC')->take($limit)->with('user')->get();
+        // }
 
-        return response([
-            'leaderboard'=>$users,
-        ], 200) ;
+        // return response([
+        //     'leaderboard'=>$users,
+        // ], 200) ;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -27,25 +27,7 @@ Route::middleware('auth:sanctum')->group(function () {
         return 'sample';
     });
 
-    Route::get('/leaderboard', function () {
-        $type = request()->get('type') ?? 'topic';
-        $topicId = request()->get('topic_id');
-        $limit= request()->get('limit') ?? 10;
-        $users = null;
-        if ($type != 'topic') {
-            $users = User::withSum('scores', 'score')->orderBy('scores_sum_score', 'DESC')->take($limit)->get();
-        } else {
-            if ($topicId == null) {
-                return ErrorHelper::sendError(404);
-            }
-            $users = Topic::find($topicId)->scores()->orderBy('score', 'DESC')->take($limit)->with('user')->get();
-        }
-
-        return response([
-            'leaderboard'=>$users,
-        ], 200) ;
-    }
-    });
+    Route::get('/leaderboard', [LeadboardController::class, 'getLeaderboard']);
     // end of private access
 });
 
