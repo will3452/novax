90ec1900c8c26f88faa7934cbede65fde181dbfb
William Galas <williamgalas2@gmail.com>
2022-03-02 17:51:49 +0800

    wip

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Http\Controllers\LoginController as ControllersLoginController;
+
+class LoginController extends ControllersLoginController
+{
+
+}
