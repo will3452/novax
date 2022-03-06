e84df25c24f0369de60e40690339ab9b7f6273e9
William Galas <williamgalas2@gmail.com>
2022-03-06 23:37:52 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
     <div class="hero min-h-screen bg-base-200">
         <div class="hero-content text-center">
           <div class="max-w-md">
-            <h1 class="text-5xl font-bold">{{config('app.name')}}</h1>
+            <h1 class="text-5xl font-bold">Web Based Modular Learning System</h1>
             <p class="py-6">{{ nova_get_setting('introduction', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quis provident amet possimus rem, alias architecto distinctio, iure magni enim eius recusandae, quisquam reiciendis obcaecati eligendi dolorem ab deleniti labore?') }}</p>
             <a class="btn btn-primary" href="/about">Read More</a>
           </div>
