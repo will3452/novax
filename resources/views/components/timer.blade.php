fdf9ba770db532a3687fb9402809456279f011a1
William Galas <williamgalas2@gmail.com>
2022-03-08 01:11:38 +0800

    wip

--- resources/views/components/timer.blade.php
+++ resources/views/components/timer.blade.php
@@ -1,6 +1,6 @@
 @props(['rt'])
 <div class="text-center">
-    <div>
+    <div id="hm">
         <div class="text-xs">
             (HH:MM)
         </div>
@@ -9,12 +9,20 @@
             <span id="mm">{{\Str::padLeft($rt[1], 2, 0)}}</span>
         </span>
     </div>
+    <div id="sec">
+        <div class="text-xs text-center text-red-500 hidden">
+            <span id="ss"></span> s
+        </div>
+    </div>
 </div>
 
 <script>
     window.onload = function () {
         const hh = document.getElementById('hh');
         const mm = document.getElementById('mm');
+        const hm = document.getElementById('hm');
+        const sec = document.getElementById('sec');
+        const ss = document.getElementById('ss');
         const form = document.getElementById('answerForm');
         let hv = hh.innerText;
         let mv = mm.innerText;
@@ -25,6 +33,11 @@
                 //examination is done!
                 form.submit();
                 clearInterval(ctimer);
+            }
+
+            if (mv<= 10 && hv <= 0) {
+                hm.classList.add('hidden');
+                sec.classList.remove('hidden');
             }
 
             if (hv >= 1 && mv < 1) {
@@ -41,6 +54,7 @@
 
             if (sv > 0) {
                 sv --;
+                sec.innerText = sv;
             }//
 
             console.log(sv);
