e005952fb746c67332a22d6f29aa0370220b45ab
William Galas <williamgalas2@gmail.com>
2022-03-11 23:50:54 +0800

    Update QuestionObserver.php

--- app/Observers/QuestionObserver.php
+++ app/Observers/QuestionObserver.php
@@ -13,18 +13,18 @@ class QuestionObserver
 
             if (isset($question->w_1)) {
                 $arr[] = $question->w_1;
-                unset($question->w_1);
             }
 
             if (isset($question->w_2)) {
                 $arr[] = $question->w_2;
-                unset($question->w_2);
             }
 
             if (isset($question->w_3)) {
                 $arr[] = $question->w_3;
-                unset($question->w_3);
             }
+            unset($question->w_1);
+            unset($question->w_2);
+            unset($question->w_3);
 
             $str = implode(Question::SEPARATOR, $arr);
 
