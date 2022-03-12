f2d946cad3989331cf0c6cbe8b488b20326efc73
William Galas <williamgalas2@gmail.com>
2022-03-13 00:57:28 +0800

    Update module-data-item.blade.php

--- resources/views/components/module-data-item.blade.php
+++ resources/views/components/module-data-item.blade.php
@@ -10,59 +10,59 @@
                 @activity($model)
                     <span class="text-xs text-red-600"> - No Grade.</span>
                 @else
-                    <span class="text-xs text-red-600"> - Opened.</span>
+                    @if ($active)
+                        <span class="text-xs text-red-600"> - Opened.</span>
+                    @endif
                 @endactivity
 
 
         @endif
     @else
 
-    @if ($active)
-        @if ($takeable)
-            @student
-                @if (! $model->isExpired() && ! is_null($score))
-                    <a href="{{$action}}" class="ml-5 btn btn-xs btn-primary">{{$proceedText}}</a>
+        @if ($active)
+            @if ($takeable)
+                @student
+                    @if (! $model->isExpired() && ! is_null($score))
+                        <a href="{{$action}}" class="ml-5 btn btn-xs btn-primary">{{$proceedText}}</a>
+                    @else
+                        @activity($model)
+                            @project($model)
+                                <a href="{{$action}}" class="ml-5 btn btn-xs btn-primary">{{$proceedText}}</a>
+                                @else
+                                    @if (! $model->isExpired() &&  is_null($score))
+                                        <a href="{{$action}}" class="ml-5 btn btn-xs btn-primary">{{$proceedText}}</a>
+                                    @else
+                                        <span class="text-red-600 text-xs">
+                                            Time is over
+                                        </span>
+                                    @endif
+                            @endproject
+                        @else
+                        <a href="{{$action}}" class="ml-5 btn btn-xs btn-primary">{{$proceedText}}</a>
+                        @endactivity
+                    @endif
                 @else
                     @activity($model)
-                        @project($model)
-                            <a href="{{$action}}" class="ml-5 btn btn-xs btn-primary">{{$proceedText}}</a>
-                            @else
-                                @if (! $model->isExpired() &&  is_null($score))
-                                    <a href="{{$action}}" class="ml-5 btn btn-xs btn-primary">{{$proceedText}}</a>
-                                @else
-                                    <span class="text-red-600 text-xs">
-                                        Time is over
-                                    </span>
-                                @endif
-                        @endproject
+                        <span class="text-red-600">
+                                - No Grade.
                     @else
-                    <a href="{{$action}}" class="ml-5 btn btn-xs btn-primary">{{$proceedText}}</a>
+                            <svg class="inline" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                        </span>
                     @endactivity
-                @endif
+                @endstudent
             @else
-               @activity($model)
-               <span class="text-red-600">
-                    - No Grade.
+                @activity($model)
+                    @if (! is_null($score))
+                        <span class="text-xs font-bold"> - score:{{$score}}</span>
+                    @else
+                        <span class="text-xs text-red-600"> - No Grade.</span>
+                    @endif
                 @else
-                    <svg class="inline" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-                </span>
-               @endactivity
-            @endstudent
-        @else
-            @activity($model)
-                @if (! is_null($score))
-                    <span class="text-xs font-bold"> - score:{{$score}}</span>
-                @else
-                    <span class="text-xs text-red-600"> - No Grade.</span>
-                @endif
-            @else
-                @if (! is_null($score))
-                    <span class="text-xs font-bold"> - score:{{$score}}</span>
-                @else
-                    <span class="text-xs text-red-600"> - Opened</span>
-                @endif
-            @endactivity
+                    @if($active)
+                        <span class="text-xs text-red-600"> - Opened</span>
+                    @endif
+                @endactivity
+            @endif
         @endif
-    @endif
     @endteacher
 </li>
