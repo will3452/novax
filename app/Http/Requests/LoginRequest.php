bdab5f07b2df3d74c309eaf7396b53a910d0884b
William Galas <williamgalas2@gmail.com>
2022-03-16 15:52:10 +0800

    Update LoginRequest.php

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -24,7 +24,7 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => ['email', 'required', 'unique:users,email'],
+            'email' => ['email', 'required', 'exists:users,email'],
             'password' => 'required',
         ];
     }
