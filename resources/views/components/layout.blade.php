1c856d6b64b3152c243c5faea68830bdd76b8ede
William Galas <williamgalas2@gmail.com>
2022-03-17 23:43:01 +0800

    wip

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
     <title>{{config('app.name', 'Nuwang')}}</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@1.16.2/dist/full.css" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
+    <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 <body>
     {{$slot}}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,27 @@
 <x-layout>
-    <div class="w-screen h-screen flex justify-center items-center bg-gray-100">
-        <h1 class="text-3xl uppercase font-bold text-gray-700">
-            &lt;Under Development /&gt;
-        </h1>
+    <div class="w-screen h-screen flex justify-center items-center bg-gray-100"
+    x-data="{
+        screenWidth:0,
+        screenHeight:0,
+        change() {
+            let btn = this.$refs.button;
+            btn.style.top  = `${Math.floor(Math.random() * this.screenHeight) - 10}px`;
+            btn.style.left  = `${Math.floor(Math.random() * this.screenWidth) - 10}px`;
+            console.log(btn)
+        },
+        init() {
+            this.screenWidth= window.innerWidth;
+            this.screenHeight = window.innerHeight;
+        }
+    }"
+    >
+        <div class="text-center">
+            <h1 class="text-3xl uppercase font-bold text-gray-700">
+                Yourzaj.xyz
+            </h1>
+            <a x-on:mouseover="change" x-ref="button" href="/admin/login" class="fixed bg-gray-800 p-2 rounded-lg text-white uppercase font-bold">
+                Administrator
+            </a>
+        </div>
     </div>
 </x-layout>
