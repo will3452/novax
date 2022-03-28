fab32a5ca0a7b159600bd0b02e39f7f1f5771325
William Galas <williamgalas2@gmail.com>
2022-03-29 06:49:08 +0800

    wip

--- resources/views/take_done.blade.php
+++ resources/views/take_done.blade.php
@@ -3,12 +3,12 @@
         Your got : {{$score}}
     </x-page-title>
     @foreach ($record->answers as $a)
-        <div class="my-2 border-2 rounded p-4 {{\App\Models\Question::isCorrect($a->question->id, $a->value) ? 'bg-green-300' : 'bg-red-300'}}">
+        <div class="my-2 border-2 rounded p-4">
             <div>
                 <span class="font-bold">Question: </span>{{$a->question->question}}
             </div>
-            <div>
-                <span class="font-bold">Your Answer: </span>{{$a->value}}
+            <div class="{{\App\Models\Question::isCorrect($a->question->id, $a->question->type === \App\Models\Question::TYPE_MULTIPLE_ANSWER ? explode(',', $a->value) : $a->value) ? 'bg-green-300' : 'bg-red-300'}} p-1">
+                <span class="font-bold ">Your Answer: </span>{{$a->value}}
             </div>
         </div>
     @endforeach
