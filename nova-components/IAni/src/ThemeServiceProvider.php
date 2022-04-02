febd45668bc24e3d0299c48ed29a2297a1d2c89d
William Galas <williamgalas2@gmail.com>
2022-04-02 16:57:44 +0800

    wip

---
+++ nova-components/IAni/src/ThemeServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Elezerk\IAni;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::booted(function () {
+            Nova::theme(asset('/elezerk/i-ani/theme.css'));
+        });
+
+        $this->publishes([
+            __DIR__.'/../resources/css' => public_path('elezerk/i-ani'),
+        ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
