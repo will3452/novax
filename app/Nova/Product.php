cab4abdc812083f6ea7ac189436243e3ae3abe3d
William Galas <williamgalas2@gmail.com>
2022-04-07 15:25:40 +0800

    Update Product.php

--- app/Nova/Product.php
+++ app/Nova/Product.php
@@ -57,7 +57,7 @@ class Product extends Resource
             Textarea::make('Description')
                 ->alwaysShow(),
             Text::make('Image', function () {
-                return "<img style='width:20px;height:20px;object-fit:contain;' src='/$this->image' />";
+                return "<a href='/$this->image'><img style='width:20px;height:20px;object-fit:contain;' src='/$this->image' /></a>";
             })->asHtml(),
             Text::make('Category'),
             Currency::make('Price')
