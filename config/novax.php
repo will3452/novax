f23867569ce80695b09cba64fa41ea89f50b7e25
William Galas <williamgalas2@gmail.com>
2022-04-21 02:45:02 +0800

    Update novax.php

--- config/novax.php
+++ config/novax.php
@@ -3,7 +3,7 @@
 
 return [
     'role_enabled' => false,
-    'time_enabled' => true,
+    'time_enabled' => false,
     'back_up_enabled' => false,
     'setting_enabled' => true,
     'profile_enabled' => true,
