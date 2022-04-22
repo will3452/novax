1909f1d6d097462497f13f3b36143ef2122f1abb
William Galas <williamgalas2@gmail.com>
2022-04-23 04:08:20 +0800

    Update new-sidebar.blade.php

--- resources/views/components/new-sidebar.blade.php
+++ resources/views/components/new-sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
-      <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="#">
-        <span class="ms-1 font-weight-bold text-white">Online Examination System</span>
+        <span class="font-weight-bold text-white">
+            <img style="width:30px; height:30px;" src="/logo.png"/> Online Examination System</span>
       </a>
     </div>
     <hr class="horizontal light mt-0 mb-2">
