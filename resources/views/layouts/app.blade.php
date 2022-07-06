4a2d370687c3be3130da82e120bef94403da1dac
will3452 <williamgalas2@gmail.com>
2022-07-06 20:33:12 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
 <body>
     @include('sweetalert::alert')
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background: #d3e8ff">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm" >
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     <img src="/img/logo.png" alt="" style="width:100px">
