455b17cbce0199096d8ac6d57f5462260a264a3b
will3452 <williamgalas2@gmail.com>
2022-10-19 22:37:25 +0800

    Update User.php

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -55,7 +55,7 @@ class User extends Resource
     public function fields(Request $request)
     {
         return [
-            ID::make()->sortable(),
+            // ID::make()->sortable(),
 
             Select::make('Account Type', 'type')
                 ->sortable()
