104272d286f6493d23bc0aa575f616e30fe74c18
will3452 <williamgalas2@gmail.com>
2022-11-08 04:19:53 +0800

    wip

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -27,6 +27,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
 
         NovaSettings::addSettingsFields([
             Image::make('Logo'),
+            Text::make('App Name'),
+            Text::make('App Version'),
+            Text::make('App Footer'),
         ]);
     }
 
--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,5 +1,8 @@
 <p class="mt-8 text-center text-xs text-80">
-    {{nova_get_setting('footer_text')}}
+    {{nova_get_setting('app_footer')}}
+</p>
+<p class="mt-4 text-center text-xs text-80">
+    v{{nova_get_setting('app_version')}}
 </p>
 {{-- <nova-dark-theme-toggle></nova-dark-theme-toggle> --}}
 {{-- <nova-dark-theme-toggle label="{{ __('Dark Theme') }}"></nova-dark-theme-toggle> --}}
