ed782295c539414dcf92cda878fb33f902b4f3e1
will3452 <williamgalas2@gmail.com>
2022-11-09 05:32:01 +0800

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::before(function ($user, $ability) {
-            return $user->hasRole(Role::SUPERADMIN) ? true : null;
+            return true;
         });
     }
 }
