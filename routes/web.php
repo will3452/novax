e356c7c235cf1be308cb4bca0bd8c327f9577153
will3452 <williamgalas2@gmail.com>
2022-11-18 05:58:17 +0800

    wip

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/', function () {
-    return redirect()->to('/login');
+    return view('welcome');
 });
 
 // Route::get('/register', [RegisterController::class, 'registrationPage']);
