42fbcefee71eb8be63b01c71cd46b655e2c91ec0
will3452 <williamgalas2@gmail.com>
2022-12-21 20:44:11 +0800

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,16 @@
-<div class=" bg-base-100 text-center text-2xl font-bold my-4">
-    Memory Enhancer with Health Monitoring
+<div class="bg-white">
+    <div class=" bg-base-100 text-center text-2xl font-bold my-4">
+        Memory Enhancer with Health Monitoring
+    </div>
+    <ul class=" bg-base-100 items-center flex justify-center">
+        @if (! Auth::check())
+                <li><a class="btn btn-sm mx-2 btn-link" href="/register">Register</a></li>
+                <li><a class="btn btn-sm mx-2 btn-link" href="/login">Login</a></li>
+            @else
+                <li><a class="btn btn-sm mx-2 btn-link" href="/home">Home</a></li>
+                <li><a class="btn btn-sm mx-2 btn-link" href="/games">Games</a></li>
+                <li><a class="btn btn-sm mx-2 btn-link" href="/profile">Profile</a></li>
+                <li><a class="btn btn-sm mx-2 btn-link" href="/logout">Logout</a></li>
+            @endif
+      </ul>
 </div>
-<ul class=" bg-base-100 items-center flex justify-center">
-    @if (! Auth::check())
-            <li><a class="btn btn-sm mx-2 btn-link" href="/register">Register</a></li>
-            <li><a class="btn btn-sm mx-2 btn-link" href="/login">Login</a></li>
-        @else
-            <li><a class="btn btn-sm mx-2 btn-link" href="/home">Home</a></li>
-            <li><a class="btn btn-sm mx-2 btn-link" href="/games">Games</a></li>
-            <li><a class="btn btn-sm mx-2 btn-link" href="/profile">Profile</a></li>
-            <li><a class="btn btn-sm mx-2 btn-link" href="/logout">Logout</a></li>
-        @endif
-  </ul>
