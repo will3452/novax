c94c137c9ca8e3c460b42ce1cbcbd211f19854db
William Galas <williamgalas2@gmail.com>
2023-04-08 16:43:02 +0800

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function diaries () {
+        return $this->hasMany(Diary::class, 'user_id');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,34 +1,81 @@
 <?php
 
 use App\Http\Controllers\ApiAuthenticationController;
+use App\Models\Collection;
+use App\Models\Faq;
+use App\Models\Plant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 
 //private access
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/auth-test', function () {
-        return 'authentication test';
-    });
     Route::post('/logout', [ApiAuthenticationController::class, 'logout']);
-});
 
-Route::get('/public-test', function () {
-    return 'public test';
+     // DIARIES
+     Route::prefix('/diary')->group(function () {
+        Route::get('/', function (Request $request) {
+            return auth()->user()->diaries()->latest()->get();
+         });
+
+        Route::post('/', function (Request $request) {
+            $data = $request->validate([
+                'title' => 'required',
+                'body' => 'required'
+            ]);
+
+            return auth()->user()->diaries()->create($data);
+        });
+     });
+
+     // Collections
+
+     Route::prefix('/collections')->group(function () {
+        Route::get('/', function () {
+            return Collection::with('plant')->whereUserId(auth()->id())->latest()->get();
+        });
+
+        Route::post('/', function (Request $request) {
+            $data = $request->validate([
+                'plant_id' => ['required'],
+            ]);
+
+            $exists = Collection::whereUserId(auth()->id())->wherePlantId($data['plant_id'])->exists();
+            if ($exists) {
+                return response()->json(['message' => 'Plant was already in your collection list.'], 400);
+            }
+            $data['user_id'] = auth()->id();
+            return Collection::create($data);
+        });
+
+        Route::delete('/{plant}', function (Request $request,Plant $plant) {
+
+            $exists = Collection::whereUserId(auth()->id())->wherePlantId($plant->id)->exists();
+
+            if (!$exists) {
+                return response()->json(['message' => 'Plant not found!'], 400);
+            }
+
+            $result = Collection::whereUserId(auth()->id())->wherePlantId($plant->id)->first()->delete();
+
+            return $result;
+        });
+     });
+
+
 });
 
 
 //user authentication
 Route::post('/register', [ApiAuthenticationController::class, 'register']);
 Route::post('/login', [ApiAuthenticationController::class, 'login']);
+
+// public
+Route::get('/faq', function (Request $request) {
+    return Faq::latest()->get();
+});
+
+Route::get('/plants', function () {
+    return Plant::latest()->get();
+});
