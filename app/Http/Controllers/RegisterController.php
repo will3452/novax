92afc36a30afedb9224fcec8fe940a33d4c698e4
will3452 <williamgalas2@gmail.com>
2023-06-29 06:10:54 +0800

    wip

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\WelcomeEmail;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -17,10 +19,11 @@ class RegisterController extends Controller
         $data = request()->validate([
             'name' => 'required',
             'email' => 'required|unique:users,email',
-            'password' => 'required|confirmed|min:6'
+            'password' => 'required|confirmed|min:6',
         ]);
         $data['password'] = bcrypt($data['password']);
         $user = User::create($data);
+        Mail::to($user)->send(new WelcomeEmail());
         return back()->withSuccess('success');
     }
 }
---
+++ resources/views/welcome_email.blade.php
@@ -0,0 +1,24 @@
+@component('mail::message')
+# Welcome to tanum
+
+Subject: Welcome to our community!
+
+Dear User,
+
+We are thrilled to welcome you to our community! We are excited that you have decided to join us and become a part of our family.
+
+We know that starting something new can be overwhelming, but we want you to know that we are here to help and support you every step of the way. Whether you have questions about our platform or need assistance with anything else, our team is always here to help.
+
+As a new member of our community, you will have access to a wide range of resources and tools that will help you get the most out of your experience. From informative articles to engaging discussions, there is something for everyone here.
+
+We hope that you will take the time to explore our platform and get to know our community. We believe that you will find that we are a friendly, welcoming group of people who are passionate about what we do.
+
+Once again, welcome to our community! We look forward to getting to know you and working together to achieve great things.
+
+Best regards,
+
+Tanum
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
