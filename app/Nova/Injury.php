2f0f0643af1411d09cd62f442ff23482c69409f7
will3452 <williamgalas2@gmail.com>
2023-08-27 06:03:18 +0800

    Update Injury.php

--- app/Nova/Injury.php
+++ app/Nova/Injury.php
@@ -21,7 +21,7 @@ class Injury extends Resource
      *
      * @var string
      */
-    public static $title = 'id';
+    public static $title = 'name';
 
     /**
      * The columns that should be searched.
