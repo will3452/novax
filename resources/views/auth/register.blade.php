1016ff68962f81d007e78dd115c34daa80a7c958
will3452 <williamgalas2@gmail.com>
2023-10-29 06:52:47 +0800

    wip

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,59 +1,87 @@
 <x-layout>
 
-<div class="hero min-h-screen bg-base-200">
-    <form action="register" method="POST" class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        @csrf
-        <div class="card-body">
-        @if (session('success'))
-            <x-alert-success>
-                Registered Succesfully!
-            </x-alert-success>
-        @endif
-        <h1 class="uppercase text-center font-bold text-gray-700 text-2xl">Register</h1>
-        <div class="form-control">
-            <label class="label">
-            <span class="label-text">Name</span>
-            </label>
-            <input type="text" value="{{old('name')}}" name="name" required placeholder="name" class="input input-bordered">
-            @error('name')
-            <x-form-error>
-                {{$message}}
-            </x-form-error>
-            @enderror
+
+    <div class="md:flex md:w-screen relative" >
+        <div class="w-screen md:w-1/2 p-10 bg-red-100">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="flex items-center ">
+                    <img src="/alarm.png" class="w-20"alt="">
+                    <h1 class="font-bold text-4xl mx-4 font-mono">Police Emerge!</h1>
+                </div>
+                <h1 class="my-4 font-bold text-4xl mt-10 text-gray-800">LOGIN</h1>
+                        <input id="email" type="email" class="w-full p-4 rounded mb-4 bg-white block placeholder-red-500  @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Enter your email." required autofocus>
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <input id="password" type="password" class="w-full p-4 rounded mb-4 bg-white block placeholder-red-500 @error('password') is-invalid @enderror" name="password" required >
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                            <div class="mb-4">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                            <label class="font-bold text-red-500 underline" for="remember">
+                                {{ __('Remember Me') }}
+                            </label>
+                            </div>
+
+                        <button type="submit" class=" px-4 p-2 bg-red-600  rounded text-white uppercase font-bold hover:bg-red-800">
+                            {{ __('Login') }}
+                        </button>
+
+                        @if (Route::has('password.request'))
+                            <a class="text-red-500 font-bold block mt-4 underline" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+            </form>
         </div>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Email</span>
-            </label>
-            <input type="text" value="{{old('email')}}" name="email" required placeholder="email" class="input input-bordered">
-            @error('email')
-            <x-form-error>
-                {{$message}}
-            </x-form-error>
-            @enderror
-            </div>
-        <div class="form-control">
-            <label class="label">
-            <span class="label-text">Password</span>
-            </label>
-            <input type="password" name="password" required placeholder="password" class="input input-bordered">
-            @error('password')
-            <x-form-error>
-                {{$message}}
-            </x-form-error>
-            @enderror
+        <div class="w-screen md:w-1/2 h-screen bg-red-500 p-10">
+            <form method="POST" action="{{ route('register') }}" autocomplete="false">
+                @csrf
+                <p class="text-red-100 font-thin">
+                    Instant Aid, Anytime, Anywhere: Your
+                    Lifesaving Web Application
+                </p>
+                <h1 class="my-4 font-bold text-4xl text-red-100">SIGN UP</h1>
+                <input id="name"  type="text" class=" w-full p-4 rounded mb-4 bg-red-100 block placeholder-red-500 @error('name') is-invalid @enderror" name="name" placeholder="Enter your name." value="{{ old('name') }}" required  autofocus>
+
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <input id="email" placeholder="Enter your Email." type="email" class=" w-full p-4 rounded mb-4 bg-red-100 block placeholder-red-500 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required >
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <input id="password" placeholder="Enter your Password." type="password" class="w-full p-4 rounded mb-4 bg-red-100 block placeholder-red-500 @error('password') is-invalid @enderror" name="password" required >
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <input id="password-confirm" type="password" class="w-full p-4 rounded mb-4 bg-red-100 block placeholder-red-500" name="password_confirmation" required placeholder="Confirm Password" >
+
+                <button type="submit" class=" px-4 p-2 bg-green-600  rounded text-white uppercase font-bold hover:bg-green-800">
+                            {{ __('Register') }}
+                </button>
+            </form>
         </div>
-        <div class="form-control">
-            <label class="label">
-                <span class="label-text">Confirm Password</span>
-            </label>
-            <input type="password" placeholder="password" required name="password_confirmation" class="input input-bordered">
-        </div>
-        <div class="form-control mt-6">
-            <input type="submit" value="Register" class="btn btn-primary">
-            <a class="block text-center text-xs mt-4 underline" href="#">Already have an account?</a>
-        </div>
-        </div>
-    </form>
-</div>
+
+        <a class="block absolute bottom-5 right-5 text-white font-bold underline hover:text-xl" href="/">BACK TO HOME</a>
+    </div>
 </x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,31 @@
 <x-layout>
-    <div class="relative w-screen h-screen overflow-hidden">
+    <div class="relative w-screen h-screen overflow-hidden bg-green-600">
         <div class="flex justify-center py-4 bg-red-100 block md:hidden">
-            <img src="/alert.png" class="md:hidden block w-20 h-20 mr-10" alt="">
+            <img src="/alarm.png" class="md:hidden block w-20 h-20 mr-10" alt="">
         </div>
         <nav class="flex p-4 text-white bg-green-400 items-center justify-center md:justify-start">
-            <img src="/alert.png" class="hidden md:block w-20 h-20 mr-10" alt="">
-            <a href="" class="md:text-2xl font-mono mx-4 border-b-2">Home</a>
-            <a href="" class="md:text-2xl font-mono mx-4">News</a>
-            <a href="" class="md:text-2xl font-mono mx-4">Officers</a>
-            <a href="" class="md:text-2xl font-mono mx-4">Account</a>
+            <img src="/alarm.png" class="hidden md:block w-20 h-20 mr-10" alt="">
+            <a href="#" class="md:text-2xl font-mono mx-4 border-b-2">Home</a>
+            <a href="/news" class="md:text-2xl font-mono mx-4">News</a>
+            <a href="/officers" class="md:text-2xl font-mono mx-4">Officers</a>
+            <a href="/register" class="md:text-2xl font-mono mx-4">Account</a>
         </nav>
         <div class="hidden md:block w-screen h-screen bg-red-500 absolute rotate-45 opacity-90" style="right: -50vw; top: -150vh; height: 100vw; "></div>
-        <div class="hidden md:block w-screen h-screen bg-red-500 absolute rotate-45 opacity-90" style="bottom: -100vh; left: -50vw;"></div>
+        <div class="z-10 hidden md:block w-screen h-screen bg-red-500 absolute rotate-45 opacity-90" style="bottom: -100vh; left: -50vw;"></div>
+        <div class="px-4 md:p-8 text-center md:text-left text-red-100 z-10 relative">
+            <h3 class="text-xl  md:text-4xl font-thin mt-8 md:mb-4">
+                Welcome to
+            </h3>
+            <h1 class="text-4xl md:text-8xl font-bold text-red-100 mb-4">
+                Police Emerge!
+            </h1>
+            <h4 class="font-thin md:text-xl">
+                Instant Aid, Anytime, Anywhere: Your Lifesaving Web Application
+            </h4>
+            <a href="" class="bg-red-500 px-4 py-2 md:inline-block block rounded-full mt-4 hover:bg-red-700 text-white font-bold border-red-800 border-4">
+                REPORT
+            </a>
+        </div>
+        <img src="/police.png" alt="" class="absolute z-0 top-40 opacity-50 md:left-20">
     </div>
 </x-layout>
