7d6e66c94868a52d3386e223181bde7d6eb1e8a1
will3452 <36730333+will3452@users.noreply.github.com>
2023-12-29 05:29:43 +0800

    Update hero.blade.php

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,18 +1,18 @@
 <div class="hero p-2 py-4 md:hidden" style="height: 50vh;">
     <div class="my-10"></div>
     <livewire:time-card/>
-    <div class="text-center text-white mt-4">
-        <h3 data-aos="fade-right" class="font-inder text-sm">The Most Trusted IT Consultacy</h3>
-        <h2 data-aos="fade-left">Find All Your <span class="color-primary font-bold">IT Solutions</span> Here</h2>
+    <div class="text-center text-white mt-4  leading-loose">
+        <h3 data-aos="fade-right" class="font-inder text-sm">Leading Sustainable Global Solutions.</h3>
+        <h2 data-aos="fade-left" class="text-xl">Sustainable Solutions, <span class="color-primary font-bold">Global Mastery.</span></h2>
         <a  data-aos="fade-up"  class="transition p-2 border-2 border-primary font-bold inline-block mt-4 color-primary hover:scale-110" href="#intro">LEARN MORE</a>
     </div>
 </div>
 <div class="hero p-2 py-4 md:flex hidden w-screen justify-center items-center" style="height: 80vh;">
     <div class="w-full">
         <livewire:time-card/>
-        <div class="text-center text-white mt-8"  >
-            <h3 class="font-inder text-2xl" data-aos="fade-right">The Most Trusted IT Consultacy</h3>
-            <h2 class="text-6xl" data-aos="fade-left">Find All Your <span class="color-primary font-bold ">IT Solutions</span> Here</h2>
+        <div class="text-center text-white mt-8 leading-loose"  >
+            <h3 class="font-inder text-2xl" data-aos="fade-right">Leading Sustainable Global Solutions.</h3>
+            <h2 class="text-6xl" data-aos="fade-left">Sustainable Solutions, <span class="color-primary font-bold ">Global Mastery.</span></h2>
             <a data-aos="fade-up" class="transition px-8 p-4 tracking-widest border-2 border-primary font-bold inline-block mt-4 color-primary hover:scale-110 text-4xl" href="#intro">LEARN MORE</a>
         </div>
     </div>
