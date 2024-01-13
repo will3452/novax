a50d8342ff06476a1a8fb64961220e6849a5c276
will3452 <36730333+will3452@users.noreply.github.com>
2024-01-13 20:26:53 +0800

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,12 @@
 <x-layout>
     <div class="w-screen h-screen flex justify-center items-center bg-gray-100">
-        <h1 class="text-3xl uppercase font-bold text-gray-700">
-            &lt;Under Development /&gt;
-        </h1>
+        <div>
+            <h1 class="text-3xl uppercase font-bold text-gray-700">
+                &lt; Tourism Website for Parañaque City/&gt;
+            </h1>
+            <div class="text-center upperacase">
+                Under development <a class="underline" href="/admin">Click here Visit Admin</a>
+            </div>
+        </div>
     </div>
 </x-layout>
