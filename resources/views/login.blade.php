f5f2c955790fc23dcff602be7c94ad0ee41ba0e1
william galas <wgalas@centralizedinc.com>
2024-03-23 23:23:59 +0800

    wip

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,6 +2,7 @@
     <div class=" w-screen max-w-[900px] mx-auto p-4">
         <x-error />
         <form action="{{route('nova.login')}}" method="POST">
+            <h1 class="text-2xl font-bold" >LOGIN</h1>
             @csrf 
             <input class="p-4 w-full mt-4 border-2" type="email" name="email" placeholder="Enter your Email" autofocus>
             <input class="p-4 w-full mt-4 border-2" type="password" name="password" placeholder="Enter your Password" autofocus>
