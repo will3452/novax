b7a624a4eb60c48ba3fd0863372b1eed3a0a24b0
william galas <wgalas@centralizedinc.com>
2024-04-06 08:58:06 +0800

    wiP

--- app/Nova/Actions/UpdateBooking.php
+++ app/Nova/Actions/UpdateBooking.php
@@ -19,7 +19,7 @@ class UpdateBooking extends Action
 
     public function escalateBookings ($booking) {
         $slot = Slot::whereIsAvailable(true)->first(); 
-        if (! $slot || count($slot) == 1) {
+        if (! $slot && $slot->driver_id == auth()->id()) {
             $booking->update([
                 'status' => 'Rejected',
                 'driver_id' => 1, // admin 
