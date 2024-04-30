efd8b36c7f9122d120c654a1ee2bfb2849ce4815
william galas <wgalas@centralizedinc.com>
2024-04-30 10:11:43 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,7 @@
             <a href="/news" class="md:text-lg font-mono mx-4">News</a>
             <a href="/organization" class="md:text-lg font-mono mx-4">Officers</a>
             <a href="/register" class="md:text-lg font-mono mx-4">Account</a>
-            <a href="https://www.tsu.edu.ph/contact-us/emergency-contact-numbers" target="_blank" class="md:text-lg font-mono mx-4">Other Emergency Hotlines</a>
-        </nav>
+            </nav>
         <div class="hidden md:block w-screen h-screen bg-blue-900 absolute rotate-45 opacity-90" style="right: -50vw; top: -150vh; height: 100vw; "></div>
         <div class="z-10 hidden md:block w-screen h-screen bg-blue-900 absolute rotate-45 opacity-90" style="bottom: -100vh; left: -50vw;"></div>
         <div x-data="{token: null}" x-init="token = localStorage.getItem('police-emerge-token')" class="px-4 md:p-8 text-center md:text-left text-blue-900 z-10 relative">
@@ -23,9 +22,12 @@
             <h4 class="font-thin md:text-xl">
                 Instant Aid, Anytime, Anywhere in Brgy. San Miguel, Tarlac.
             </h4>
-            <a :href="token == null || token == '' ? '/home': '/anonymous'" class="bg-blue-900 px-4 py-2 md:inline-block block rounded-full mt-4 hover:bg-blue-900 text-white font-bold border-blue-900 border-4">
+            <a :href="token == null || token == '' ? '/home': '/anonymous'" class=" bg-blue-900 px-4 py-2 md:inline-block block rounded-full mt-4 hover:bg-blue-900 text-white font-bold border-blue-900 border-4">
                 REPORT
-            </a>
+            </a> 
+            <div class="mt-2">
+                <a href="https://www.tsu.edu.ph/contact-us/emergency-contact-numbers" target="_blank" class="inline-block shadow-md p-1 bg-white font-mono font-bold">Other Emergency Hotlines</a>
+            </div>
         </div>
         <img src="/police.png" alt="" class="absolute z-0 top-40 opacity-50 md:left-20">
     </div>
