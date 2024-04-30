04eeb114d7aa4cb86d6a3c282bd12b92b587b9c9
william galas <wgalas@centralizedinc.com>
2024-04-30 10:04:20 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,13 @@
         <div class="flex justify-center py-4 bg-blue-100 block md:hidden">
             <img src="/alarm.png" class="md:hidden block w-20 h-20 mr-10" alt="">
         </div>
-        <nav class="flex p-4 text-blue-900 bg-blue-400 items-center justify-center md:justify-start">
+        <nav class="flex p-4 text-white bg-blue-900 items-center justify-center md:justify-start">
             <img src="/alarm.png" class="hidden md:block w-20 h-20 mr-10" alt="">
-            <a href="#" class="md:text-2xl font-mono mx-4 border-b-2">Home</a>
-            <a href="/news" class="md:text-2xl font-mono mx-4">News</a>
-            <a href="/organization" class="md:text-2xl font-mono mx-4">Officers</a>
-            <a href="/register" class="md:text-2xl font-mono mx-4">Account</a>
+            <a href="#" class="md:text-lg font-mono mx-4 border-b-2">Home</a>
+            <a href="/news" class="md:text-lg font-mono mx-4">News</a>
+            <a href="/organization" class="md:text-lg font-mono mx-4">Officers</a>
+            <a href="/register" class="md:text-lg font-mono mx-4">Account</a>
+            <a href="https://www.tsu.edu.ph/contact-us/emergency-contact-numbers" target="_blank" class="md:text-lg font-mono mx-4">Other Emergency Hotlines</a>
         </nav>
         <div class="hidden md:block w-screen h-screen bg-blue-900 absolute rotate-45 opacity-90" style="right: -50vw; top: -150vh; height: 100vw; "></div>
         <div class="z-10 hidden md:block w-screen h-screen bg-blue-900 absolute rotate-45 opacity-90" style="bottom: -100vh; left: -50vw;"></div>
@@ -20,7 +21,7 @@
                 Police Emerge!
             </h1>
             <h4 class="font-thin md:text-xl">
-                Instant Aid, Anytime, Anywhere: Your Lifesaving Web Application
+                Instant Aid, Anytime, Anywhere in Brgy. San Miguel, Tarlac.
             </h4>
             <a :href="token == null || token == '' ? '/home': '/anonymous'" class="bg-blue-900 px-4 py-2 md:inline-block block rounded-full mt-4 hover:bg-blue-900 text-white font-bold border-blue-900 border-4">
                 REPORT
