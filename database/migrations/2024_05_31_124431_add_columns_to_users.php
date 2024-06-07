0f5b717cc560e1a4969bbebf1ad734329b5376ad
william galas <wgalas@centralizedinc.com>
2024-06-08 07:38:43 +0800

    Update 2024_05_31_124431_add_columns_to_users.php

--- database/migrations/2024_05_31_124431_add_columns_to_users.php
+++ database/migrations/2024_05_31_124431_add_columns_to_users.php
@@ -14,6 +14,7 @@ class AddColumnsToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->string('type')->nullable(); 
             $table->string('number')->nullable(); 
             $table->string('course')->nullable(); 
             $table->text('signature')->nullable(); 
