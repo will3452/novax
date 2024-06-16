4f0539e4e7497d6bfd15efbce0bbf29ae84ca1ad
william galas <wgalas@centralizedinc.com>
2024-06-16 10:41:13 +0800

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="flex justify-center items-center w-screen h-screen">
         <div class="text-center mt-4">
-            <h1 class="text-4xl">Welcome to Intership Management System</h1>
+            <h1 class="text-4xl">Welcome to Internship Management System</h1>
             <h2 class="text-lg">Bridging the gap between education and real-world skills</h2>
             <a class="inline-block rounded-full font-bold text-white m-2 p-2 px-4 bg-blue-400" href="/app/login">LOGIN</a>
             <a class="inline-block rounded-full font-bold text-white m-2 p-2 px-4 bg-yellow-600" href="/register-hte">REGISTER AS HTE</a>
