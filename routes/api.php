4e32e7f868eb739f9f47ed33c3fc6bedd1142e91
william galas <wgalas@centralizedinc.com>
2024-07-01 07:17:50 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -42,3 +42,31 @@ Route::post('/login', [ApiAuthenticationController::class, 'login']);
 Route::resource('products', ProductController::class); 
 Route::resource('stores', StoreController::class); 
 Route::resource('store-categories', StoreCategoryController::class); 
+
+Route::post('/generate-token', function (Request $request) {
+    $header = base64_encode("pk_test_lbqjzyCPiyBmvaGtRrMGI6".":"); 
+    $cardObj = [
+        'card' => [
+            'name' => $request->name, 
+            'number' => $request->number, 
+            'exp_month' => $request->exp_month, 
+            'exp_year' => $request->exp_year, 
+            'cvc' => $request->cvc, 
+        ]
+    ];
+    
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, "https://api.magpie.im/v1/tokens");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($cardObj));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Content-Type: application/json",
+        "Accept: application/json",
+        "Authorization: Basic " . $header
+      ));
+      $response = curl_exec($ch);
+    curl_close($ch);
+    return $response; 
+}); 
