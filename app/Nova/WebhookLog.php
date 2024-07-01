27f660b4067c494a6755f764970564c7b0f3c399
william galas <wgalas@centralizedinc.com>
2024-07-01 15:48:35 +0800

    Update WebhookLog.php

--- app/Nova/WebhookLog.php
+++ app/Nova/WebhookLog.php
@@ -10,6 +10,7 @@ use Laravel\Nova\Http\Requests\NovaRequest;
 
 class WebhookLog extends Resource
 {
+    public static $group = 'Security';
     /**
      * The model the resource corresponds to.
      *
