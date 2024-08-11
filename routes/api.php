72eea1c2c491a387600359c034cbea9486b14ad6
william galas <wgalas@centralizedinc.com>
2024-08-11 21:31:19 +0800

    wip

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ServiceController;
 use App\Models\CartItem;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,12 @@ Route::middleware('auth:sanctum')->group(function () {
         return 'authentication test';
     });
     Route::post('/logout', [ApiAuthenticationController::class, 'logout']);
+
+    Route::get('/me', function (Request $request) {
+        $user = User::find(auth()->id());
+        $user->load(['cartItems', 'bookings']);
+        return $user;  
+    });
 
     Route::resource('/bookings', BookingController::class); 
     Route::resource('/cart-items', CartController::class); 
