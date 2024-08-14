bd2861c949da3938a96e31f065783fe4114137af
william galas <wgalas@centralizedinc.com>
2024-08-15 01:32:26 +0800

    wip

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -17,7 +17,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        return Order::paginate(10); 
+        return Order::with('products')->paginate(10); 
     }
 
     /**
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -21,4 +21,8 @@ class Order extends Model
     public function user () {
         return $this->belongsTo(User::class); 
     }
+
+    public function products () {
+        return $this->belongsToMany(Product::class, 'order_items', 'order_id', 'product_id'); 
+    }
 }
