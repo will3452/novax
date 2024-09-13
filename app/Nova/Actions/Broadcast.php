bf17398b941258b63bcb5134736182edf6e9374d
william galas <wgalas@centralizedinc.com>
2024-09-13 23:17:43 +0800

    wip

--- app/Nova/Actions/Broadcast.php
+++ app/Nova/Actions/Broadcast.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Nova\Actions;
 
 use App\Models\Customer;
@@ -24,30 +23,24 @@ class Broadcast extends Action
     public function handle(ActionFields $fields, Collection $models)
     {
         $ch = curl_init();
-        $customers = Customer::get()->pluck('phone'); 
+        $customers = Customer::get(); 
         foreach($models as $model) {
-            $phones = []; 
             foreach($customers as $customer) {
-                $array = explode('-', $customer); 
-                $phone = implode('', $array); 
-                array_push($phones, $phone); 
+                $parameters = array(
+                    'apikey' => nova_get_setting('SMS_KEY', env('SMS_KEY')), //Your API KEY
+                    'number' => $customer->phone,
+                    'message' => $model->content,
+                    'sendername' => nova_get_setting('SENDER_NAME', 'GOTRAKTORA'), 
+                );
+
+                curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/messages' );
+                curl_setopt( $ch, CURLOPT_POST, 1 );
+
+                curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
+    
+                curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+                $output = curl_exec( $ch );
             }
-
-            
-            $parameters = array(
-                'apikey' => nova_get_setting('SMS_KEY', env('SMS_KEY')), //Your API KEY
-                'number' => implode(',', $phones),
-                'message' => $model->content,
-                'sendername' => nova_get_setting('SENDER_NAME', 'GOTRAKTORA'), 
-            );
-
-            curl_setopt( $ch, CURLOPT_URL,'https://semaphore.co/api/v4/messages' );
-            curl_setopt( $ch, CURLOPT_POST, 1 );
-
-            curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
-
-            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-            $output = curl_exec( $ch );
         }
 
         curl_close($ch); 
