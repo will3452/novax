8ea351577de2eee54d1b200f6b6dd61a0f008826
william galas <wgalas@centralizedinc.com>
2024-11-01 11:02:52 +0800

    wip

--- resources/views/finance_report.blade.php
+++ resources/views/finance_report.blade.php
@@ -57,7 +57,7 @@
                         $totalExpenses += $expenses;
                         $totalOfferings += $offerings;
                         $totalTithes += $tithes;
-                        $tot = ($tithes + $totalOfferings) * .10;
+                        $tot = ($tithes + $offerings) * .10;
                         $totalTot += $tot;
                         $gross = $offerings + $tithes;
                         $totalGross += $gross;
