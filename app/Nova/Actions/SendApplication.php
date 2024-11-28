bea5ae3d31a493678a202da5331f394eb0b6ff91
wgalas <williamgalas2@gmail.com>
2024-11-28 09:55:26 +0800

    wip

--- app/Nova/Actions/SendApplication.php
+++ app/Nova/Actions/SendApplication.php
@@ -24,19 +24,21 @@ class SendApplication extends Action
      */
     public function handle(ActionFields $fields, Collection $models)
     {
-        $model = $models[0];
+        foreach ($models as $model) {
+            if ($model->status == 'Taken') return Action::danger('This title was already assigned.');
+            $exists = TitleApplication::whereStudentId(auth()->id())->whereSectionId($model->section_id)->whereStatus('APPROVED')->exists();
+            $submitted = TitleApplication::whereStudentId(auth()->id())->whereSectionId($model->section_id)->whereTitleId($model->id)->exists();
+            $alreadyApplied = TitleApplication::whereStudentId(auth()->id())->whereStatus('PENDING')->exists();
+            if ($submitted) return Action::danger("You have already applied to this title.");
+            if ($exists || $alreadyApplied) return Action::danger("You have already applied to another title in this section. you may contact your adviser to remove your application.");
+            TitleApplication::create([
+                'title_id' => $model->id,
+                'section_id' => $model->section_id,
+                'student_id' => auth()->id(),
+            ]);
+        }
 
-        if ($model->status == 'Taken') return Action::danger('This title was already assigned.');
-        $exists = TitleApplication::whereStudentId(auth()->id())->whereSectionId($model->section_id)->whereStatus('APPROVED')->exists();
-        $submitted = TitleApplication::whereStudentId(auth()->id())->whereSectionId($model->section_id)->whereTitleId($model->id)->exists();
-        $alreadyApplied = TitleApplication::whereStudentId(auth()->id())->whereStatus('PENDING')->exists();
-        if ($submitted) return Action::danger("You have already applied to this title.");
-        if ($exists || $alreadyApplied) return Action::danger("You have already applied to another title in this section. you may contact your adviser to remove your application.");
-        TitleApplication::create([
-            'title_id' => $model->id,
-            'section_id' => $model->section_id,
-            'student_id' => auth()->id(),
-        ]);
+
     }
 
     /**
