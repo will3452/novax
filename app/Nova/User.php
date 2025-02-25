e4235fbc73d438035ce29b7022264c767c7c7322
wgalas <williamgalas2@gmail.com>
2025-02-25 19:27:13 +0800

    wip:

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -25,7 +25,7 @@ class User extends Resource
 
     public static function authorizedToCreate(Request $request)
     {
-        return false;
+        return auth()->id() == 1;
     }
 
     public function authorizedToDelete(Request $request)
