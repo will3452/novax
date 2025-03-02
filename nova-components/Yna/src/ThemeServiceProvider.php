d79f57d40893b838aeb8780e64c41dce3f7da8ff
wgalas <williamgalas2@gmail.com>
2025-03-02 10:07:58 +0800

    wip: change theme

---
+++ nova-components/Yna/src/ThemeServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Elezerk\Yna;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Nova\Nova;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::booted(function () {
+            Nova::theme(asset('/elezerk/yna/theme.css'));
+        });
+
+        $this->publishes([
+            __DIR__.'/../resources/css' => public_path('elezerk/yna'),
+        ], 'public');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
