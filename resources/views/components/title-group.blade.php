78c16a83a42b3d29f61f1dcc4f3bbf773a85bd63
wgalas <williamgalas2@gmail.com>
2025-03-08 22:03:39 +0800

    wip:

--- resources/views/components/title-group.blade.php
+++ resources/views/components/title-group.blade.php
@@ -1,16 +1,18 @@
 @props(['title'])
 <div class="row mt-4">
-    <div class="col-md-3" >
-        <div class="card">
-            <div class="card-header">
-                Status
-            </div>
-            <div class="card-body">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M10 20.777a9 9 0 0 1-2.48-.969M14 3.223a9.003 9.003 0 0 1 0 17.554m-9.421-3.684a9 9 0 0 1-1.227-2.592M3.124 10.5c.16-.95.468-1.85.9-2.675l.169-.305m2.714-2.941A9 9 0 0 1 10 3.223"/><path d="m9 12l2 2l4-4"/></g></svg>
-                {{$title->group->status}}
+    @if (! $title->group->verdict)
+        <div class="col-md-3" >
+            <div class="card">
+                <div class="card-header">
+                    Status
+                </div>
+                <div class="card-body">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M10 20.777a9 9 0 0 1-2.48-.969M14 3.223a9.003 9.003 0 0 1 0 17.554m-9.421-3.684a9 9 0 0 1-1.227-2.592M3.124 10.5c.16-.95.468-1.85.9-2.675l.169-.305m2.714-2.941A9 9 0 0 1 10 3.223"/><path d="m9 12l2 2l4-4"/></g></svg>
+                    {{$title->group->status}}
+                </div>
             </div>
         </div>
-    </div>
+    @endif
     <div class="col-md-3">
         <div class="card">
             <div class="card-header">
