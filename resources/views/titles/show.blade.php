53b19a4497fd1074cef4a0f9e121ae8f2098ad3f
wgalas <williamgalas2@gmail.com>
2025-03-15 10:32:14 +0800

    wip:

--- resources/views/titles/show.blade.php
+++ resources/views/titles/show.blade.php
@@ -8,7 +8,7 @@
                     </div>
                     <div class="card-body">
                        <div class="d-flex justify-content-between align-items-center">
-                        <h4>{{$title->group->code}} </h4>
+                        <h4>{{$title->status == 'APPROVED' ? $title->group->code : '-'}} </h4>
                         <span class="badge bg-primary">{{$title->status}}</span>
                        </div>
                         <div>
