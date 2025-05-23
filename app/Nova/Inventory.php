d85ca39d8efb7846cf95ffe28bf690c21975cee7
wgalas <williamgalas2@gmail.com>
2025-05-23 14:54:56 +0800

    lift rule

--- app/Nova/Inventory.php
+++ app/Nova/Inventory.php
@@ -51,10 +51,7 @@ class Inventory extends  BranchResourceFilter
     {
         return [
             BelongsTo::make('Branch', 'branch', Branch::class),
-            BelongsTo::make('Size', 'product', Product::class)
-            ->creationRules([
-                new NoDuplicateProduct(),
-            ]),
+            BelongsTo::make('Size', 'product', Product::class),
             Currency::make('Sales Price', fn () => $this->product ? $this->product->price : 0),
             Currency::make('Cost', fn () => $this->product ? $this->product->cost: 0),
             Number::make('Quantity On Hand', 'qty')->sortable(),
