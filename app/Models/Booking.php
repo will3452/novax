9c2781790fe16e8c0eae1a1ada801368f6af2c01
william galas <wgalas@ccci-tech.com>
2026-02-10 23:45:32 +0800

    wip:

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -10,20 +10,26 @@ class Booking extends Model
     use HasFactory;
 
     protected $fillable = [
-        'reference',
-        'patient_id',
-        'date',
-        'time',
-        'service_id',
-        'status',
-        'remarks', 
-    ]; 
+        "reference",
+        "patient_id",
+        "date",
+        "time",
+        "service_id",
+        "status",
+        "remarks",
+    ];
 
-    public function patient() {
-        return $this->belongsTo(User::class, 'patient_id'); 
+    protected $casts = [
+        "date" => "date",
+    ];
+
+    public function patient()
+    {
+        return $this->belongsTo(User::class, "patient_id");
     }
 
-    public function service () {
-        return $this->belongsTo(Service::class, 'service_id'); 
+    public function service()
+    {
+        return $this->belongsTo(Service::class, "service_id");
     }
 }
