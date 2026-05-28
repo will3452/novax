6d41ea46ad1e276f4638fd94252e1d0413e832a3
william galas <wgalas@ccci-tech.com>
2026-05-28 10:40:54 +0800

    wip:

--- database/migrations/2025_11_17_134233_create_products_table.php
+++ database/migrations/2025_11_17_134233_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->decimal('price', 10, 2);
             $table->string('category')->default(Product::CATEGORY_SINGLE);
             $table->string('image')->nullable();
