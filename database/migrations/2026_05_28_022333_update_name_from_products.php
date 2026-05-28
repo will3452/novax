3a89a1d9efdafe125b080fdb6f0cd19392b92812
william galas <wgalas@ccci-tech.com>
2026-05-28 10:26:51 +0800

    wip:

--- database/migrations/2026_05_28_022333_update_name_from_products.php
+++ database/migrations/2026_05_28_022333_update_name_from_products.php
@@ -14,6 +14,7 @@ class UpdateNameFromProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('name');
             $table->string('name')->nullable();
         });
     }
